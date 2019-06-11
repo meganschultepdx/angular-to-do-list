e2dd1faf219c87b33dec26276b4f206d1e957d63
Student <example@example.com>
2019-06-11 16:53:59 -0700

    add homeowrk stuuf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,42 @@
 import { Component, TRANSLATIONS } from '@angular/core';
 import { Task } from './models/task.model';
+import { AotCompiler } from '@angular/compiler';
+import { dashCaseToCamelCase } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
-  currentFocus: string = 'Angular Homework';
   currentTime = new Date();
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDay();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finsih weekend Angular home for Epicodus course'),
-    new Task('Begin brainstorming possible Javascript group Projects'),
-    new Task('Add README file to last few Angular Repos on GitHub')
+    new Task('Finsih weekend Angular home for Epicodus course', 3),
+    new Task('Begin brainstorming possible Javascript group Projects', 2),
+    new Task('Add README file to last few Angular Repos on GitHub', 2)
   ]
- 
+
+  selectedTask = null;
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+  
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }    
+  }
 }
